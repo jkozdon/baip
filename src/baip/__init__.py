f37ea0f2ed23bc06fca822dadd82bbdd679a7735
Jeremy E Kozdon <jeremy@kozdon.net>
2025-12-27 14:34:00 -0800

    fix typo
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/baip/__init__.py
+++ src/baip/__init__.py
@@ -3,7 +3,7 @@
 #       - Possible choice for resolution
 # TODO: Check if terminal
 #       - 8 cats on board (easy)
-#       - three cans in a row
+#       - three cats in a row
 # TODO: [optional] Add ability to check if move is valid
 
 from dataclasses import dataclass
